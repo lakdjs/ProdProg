6212b38a1c22ad6e1fbe899f782426d44a767f8c
praprf <fil18mot@gmail.com>
2023-12-25 02:32:53 +0300

    started kt, and finished >3

---
+++ kt_eventSystems_Filchagin/kt_eventSystems_Filchagin/AnotherClass.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace kt_eventSystems_Filchagin
+{
+    public class AnotherClass
+    {
+        public AnotherClass() 
+        {
+            EventSystem.Subscribe("AttackWaveStart", Defend);
+            EventSystem.Subscribe("AttackWaveEnd", RetireHome);
+            EventSystem.Subscribe("DateTime", Timing);
+        }
+        public void Defend(DateTime dateTime)
+        {
+            Console.WriteLine($"Сейчас {DateTime.Now}, что то произойдет ");
+        }
+        public void RetireHome(DateTime dateTime)
+        {
+            Console.WriteLine($"Домой");
+            EventSystem.Unsubscribe("AttackWaveEnd", RetireHome);
+        }
+        public void Timing(DateTime dateTime)
+        {
+            Console.WriteLine($"Времячко : {DateTime.Now}");
+        }
+    }
+}
---
+++ kt_eventSystems_Filchagin/kt_eventSystems_Filchagin/Player.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Runtime.Serialization;
+
+namespace kt_eventSystems_Filchagin
+{
+    public class Player
+    {
+        public Player()
+        {
+            EventSystem.Subscribe("AttackWaveStart", Defend);
+            EventSystem.Subscribe("AttackWaveEnd", RetireHome);
+            EventSystem.Subscribe("DateTime", Timing);
+        }
+        public void Defend(DateTime dateTime)
+        {
+            Console.WriteLine($"Сейчас {DateTime.Now}, я готов к отражению атаки монстров!");
+        }
+        public void RetireHome(DateTime dateTime)
+        {
+            Console.WriteLine($"Я успешно отбился от врагов и теперь могу вернуться домой");
+            //EventSystem.Unsubscribe("AttackWaveEnd", RetireHome);
+        }
+        public void Timing(DateTime dateTime)
+        {
+            Console.WriteLine($"Текущее время : {DateTime.Now}");
+        }
+    }
+}
